9361988e00c555e1971e131ef19ac1370e7ceba4
devyess <90464696+devyess@users.noreply.github.com>
2025-01-14 12:02:30 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_APP_BASE_URL;
 const instance = axios.create({
-      baseURL: 'http://localhost:3000',
+      baseURL: baseURL,
 });
 
 export default instance;
