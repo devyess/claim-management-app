720413f7b2cba35e8c360a1855cca1f379276ae1
devyess <90464696+devyess@users.noreply.github.com>
2025-01-14 13:04:27 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,18 @@ const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
 app.use(express.json());
 const patientRoutes = require("./routes/patient.routes");
 const insurerRoutes = require("./routes/insurer.routes");
 const connectDB = require("./config/mongo.config");
-
+if (!fs.existsSync('uploads')) {
+    fs.mkdirSync('uploads');
+  }
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
@@ -20,6 +25,6 @@ connectDB();
 app.use("/patients", patientRoutes);
 app.use("/insurers", insurerRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server is running");
 });
